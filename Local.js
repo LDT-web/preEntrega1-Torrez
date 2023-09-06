2c6d49beadfe50e47aa5ada6fd591ee453d29ac9
Lucas Torrez <ldtorrez02@gmail.com>
2023-09-05 23:31:32 -0300

    function Pantalones

--- Local.js
+++ Local.js
@@ -1,27 +1,12 @@
-// 1 hola q esta buscando tenemos remeras pantalones zapatillas
-//elegie una opcion
-//2 tenesmos en tallas s m l
-//2.5 remeras talla m no hay
-
-//3 color blanco negro gris
-
-//4 preguntar si quere algo mas
-// yo pregunto una vez empiezo a elegir y cuando termine q me pregunte si quiero algo mas y
-
 let local;
 let hola;
-let remera;
 let color;
 
 function remeras() {
   talle = prompt("tenemos en talla s, m y l ");
   if (talle == "s" || talle == "m") {
     color = prompt("tenes en color blanco y negro");
-  }
-  // else if ((talle = "m")) {
-  //   color = prompt("tenes en color blanco y negro");
-  // }
-  else {
+  } else {
     alert("en este momento no tenemos remeras en esa talla");
   }
 }
@@ -30,7 +15,26 @@ function zapatillas() {
   talle = parseInt(prompt("tenemos en talla 36, 38 y 39 "));
   if (talle == "36") {
     color = prompt("tenes en color blanco y negro");
-  } else if ((talle = "38")) {
+  } else if (talle == "38") {
+    color = prompt("tenes en color verde y azul");
+    if (color == "verde") {
+      alert("las verdes tienen un 25% de descuento");
+    } else if (color == "azul") {
+      alert("las azules tienen un 15% de descuento");
+    } else {
+      alert("esta zapatilla no tiene descuento");
+    }
+  } else {
+    alert("en este momento no tenemos zapatillas en esa talla");
+  }
+}
+function pantalones() {
+  talle = prompt("tenemos en talla 40, 42 y 44 ");
+  if (talle == "40") {
+    color = prompt("tenes en color morados y beige");
+  } else if (talle <= "39") {
+    color = prompt("tenes solo en color negro");
+  } else if (talle >= "44") {
     color = prompt("tenes en color blanco y negro");
   } else {
     alert("en este momento no tenemos zapatillas en esa talla");
@@ -51,6 +55,9 @@ while (local != "ir a caja") {
         case "remeras":
           remeras();
           break;
+        case "pantalones":
+          pantalones();
+          break;
         case "zapatillas":
           zapatillas();
           break;
@@ -61,8 +68,5 @@ while (local != "ir a caja") {
       break;
   }
 }
-// let compras
 
 alert("su compra es una " + hola + " talle " + talle + " de color " + color);
-// compra();
-// eta maal tengo q cambiar la varriable de producto q al empezar el  bucle empiesa de nuevo ey la variable producto cambia
